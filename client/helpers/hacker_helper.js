ed89ec7531be338c5c41fe1becade81cbe01bcee
Ryan Jones <ryan.joshua.jones@gmail.com>
2013-05-08 18:06:20 -0700

    added sub-partials for hacker profile page

--- client/helpers/hacker_helper.js
+++ client/helpers/hacker_helper.js
@@ -1,3 +1,27 @@
 Handlebars.registerHelper('hacker', function(input) {
   return Session.get("hacker").username;
-})
+});
+
+Meteor.startup(function() {
+  Session.set('active_section', 'profileAbout');
+
+  Template.hackerPage.profile_section = function() {
+    var page_index = Session.get('active_section');
+    return Template[page_index]();
+  };
+
+  Template.hackerPage.events = {
+    // mousedown about
+    'mousedown .profile-nav-link.about': function() {
+      Session.set('active_section', 'profileAbout');
+    },
+    // mousedown codes
+    'mousedown .profile-nav-link.codes': function() {
+      Session.set('active_section', 'profileCodes');
+    },
+    // mousedown blog
+    'mousedown .profile-nav-link.blog': function() {
+      Session.set('active_section', 'profileBlog');
+    }
+  }
+});
