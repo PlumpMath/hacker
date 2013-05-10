5ce3b23db7f92e9d6c59d419768f4e474d39ce3d
Ryan Jones <ryan.joshua.jones@gmail.com>
2013-05-09 22:26:39 -0700

    added profile styling and blog support

--- client/helpers/hacker_helper.js
+++ client/helpers/hacker_helper.js
@@ -14,7 +14,7 @@ Handlebars.registerHelper('hacker_tools', function(input) {
     html += "</ul>";
     return html;
   } else {
-    return "";
+    return "none entered";
   }
 });
 
@@ -23,8 +23,35 @@ Handlebars.registerHelper('hacker_about', function(input) {
   if (Session.get("hacker").profile.about) {
     return Session.get("hacker").profile.about;
   } else {
-    return "";
+    return "none entered";
   }
+});
+
+// blog
+Handlebars.registerHelper('hacker_blog', function(input) {
+  var hacker = Session.get("hacker").username;
+  var current_blog = Blogs.findOne({creator: hacker});
+  if (current_blog) {
+    var posts = current_blog.posts
+    var html = '<ul class="blog-posts">';
+    for (var i = 0; i < posts.length; i++) {
+      html += '<li class="post">';
+      html += '<h3 class="post-title">' + posts[i].title + '</h3>';
+      html += '<p class="post-body">' + posts[i].body.substr(0,100) +  '...</p>';
+      html += '<p class="post-date">' + posts[i].date + '</p>';
+      html += '</li>';
+    }
+    html += '</ul>';
+    var header_html = '<h2 class="blog-header">';
+    if (current_blog.title) {
+      header_html += current_blog.title + '</h2>';
+    } else {
+      header_html += hacker.username + '\'s blog</h2>';
+    }
+    return header_html + html;
+  } else {
+    return '<p class="no-blog">no posts</p>';
+  };
 });
 
 // load partials based on navigation tab clicks
@@ -42,8 +69,8 @@ Meteor.startup(function() {
       Session.set('active_section', 'profileAbout');
     },
     // mousedown codes
-    'mousedown .profile-nav-link.codes': function() {
-      Session.set('active_section', 'profileCodes');
+    'mousedown .profile-nav-link.code': function() {
+      Session.set('active_section', 'profileCode');
     },
     // mousedown blog
     'mousedown .profile-nav-link.blog': function() {
--- client/helpers/hackers.js
+++ client/helpers/hackers.js
@@ -9,7 +9,7 @@
     user_list = Meteor.users.find().fetch();
     for (i = 0; i < user_list.length; i++) {
       if (user_list[i].username) {
-        html += '<li class="hacker"><a href="/hackers/' + user_list[i].username + '">'
+        html += '<li class="hacker"><a class="hacker-link" href="/hackers/' + user_list[i].username + '">'
         html += user_list[i].username + '</a></li>'
       }
     };
