3c9e2dd86cdc306ef5d622f89b6b30686f2a8749
root <pretty=short>
2013-05-18 21:24:29 -0700

    date added to post

--- server/blog.js
+++ server/blog.js
@@ -4,10 +4,10 @@ Meteor.methods({
     if (title == "" || body == "") {
       throw new Meteor.Error(204, 'must have content to create a blog post');
     } else if (! Blogs.findOne({ creator: hacker })) {
-      return Blogs.insert({ creator: hacker, posts: [ { title: title, body: body }]});
+      return Blogs.insert({ creator: hacker, posts: [ { title: title, body: body, date: new Date() }]});
     } else {
       console.log('creating post');
-      return Blogs.update({creator: hacker}, {$push:{posts:[ { title: title, body: body }]}});
+      return Blogs.update({creator: hacker}, {$push:{posts:[ { title: title, body: body, date: new Date() }]}});
     }
   }
 });
