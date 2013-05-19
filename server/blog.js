4bf26b9644d81646281b0b9f5ec4b875812f55ca
root <pretty=short>
2013-05-18 21:14:25 -0700

    fixed error throw

--- server/blog.js
+++ server/blog.js
@@ -2,7 +2,7 @@ Meteor.methods({
   createBlogPost: function(title, body) {
     var hacker = Meteor.user().username;
     if (title == "" || body == "") {
-      return throw new Meteor.Error(204, 'must have content to create a blog post');
+      throw new Meteor.Error(204, 'must have content to create a blog post');
     } else if (! Blogs.findOne({ creator: hacker })) {
       return Blogs.insert({ creator: hacker, posts: [ { title: title, body: body }]});
     } else {
