858e7492ea488d1963418950b351d576091c7b4c
root <pretty=short>
2013-05-18 20:45:50 -0700

    create new blog if none present

--- server/blog.js
+++ server/blog.js
@@ -1,11 +1,13 @@
 Meteor.methods({
   createBlogPost: function(title, body) {
+    var hacker = Meteor.user().username;
     if (title == "" || body == "") {
-      throw new Meteor.Error(204, 'must have content to create a blog post');
+      return throw new Meteor.Error(204, 'must have content to create a blog post');
+    } else if (! Blogs.findOne({ creator: hacker })) {
+      return Blogs.insert({ creator: hacker, posts: [ { title: title, body: body }]});
     } else {
       console.log('creating post');
-      var hacker = Meteor.user().username;
-      return Blogs.update({creator: hacker}, {$push:{post: { title: title, body: body }}});
+      return Blogs.update({creator: hacker}, {$push:{posts:[ { title: title, body: body }]}});
     }
   }
 });
